4982f3a18db5ab69f08c71b30a24eee28a9ea5a3
Pratima <pratima.waghmare@logiquad.com>
2021-12-26 22:32:57 +0530

    Added login & register components

--- src/App.js
+++ src/App.js
@@ -1,24 +1,27 @@
+import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
+import Login from './components/LoginForm';
+import { Register } from './components/RegistrationForm';
 
-function App() {
+function App(props) {
+
+  const [isLogginActive, setIsLogginActive] = useState(false);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="login">
+      <div className="container">
+        {
+        isLogginActive ?
+          <Login />
+          :
+          <Register />
+        }
+      </div>
     </div>
+  </div>
   );
 }
 
